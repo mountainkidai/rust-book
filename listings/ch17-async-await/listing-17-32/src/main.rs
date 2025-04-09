5bc41d882170422a3ad5718a9dd90491282760a4
Ramiro Krapp <rkrapp@gocloud.la>
2025-04-09 19:35:03 -0300

    add ANCHOR filter-out-except-3-and-5 to listing 17-32

--- listings/ch17-async-await/listing-17-32/src/main.rs
+++ listings/ch17-async-await/listing-17-32/src/main.rs
@@ -1,5 +1,7 @@
 extern crate trpl; // required for mdbook test
 
+// ANCHOR: filter-out-except-3-and-5
+
 use trpl::StreamExt;
 
 fn main() {
@@ -16,3 +18,4 @@ fn main() {
         }
     });
 }
+// ANCHOR_END: filter-out-except-3-and-5
